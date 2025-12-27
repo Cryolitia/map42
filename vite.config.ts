66b488d3095633eaa3f220d794b5e1f7787e2f5d
Cryolitia PukNgae <cryolitia@gmail.com>
2025-12-27 12:02:26 +0800

    ci: add publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { loadEnv } from 'vite'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -10,6 +11,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/map42/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
